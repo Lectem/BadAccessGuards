79c75832b0f410aad35ff6cae800ad8640a601d2
Clément Grégoire <lectem@gmail.com>
2025-01-29 15:36:47 +0100

    Add benchmarks

---
+++ benchmarks/BenchGuardedVectorExample.cpp
@@ -0,0 +1,119 @@
+#define BAD_ACCESS_GUARDS_ENABLE 1
+#include "../examples/GuardedVectorExample.h"
+
+#include <nanobench.h>
+
+#include <vector>
+
+
+using namespace std::chrono_literals;
+const auto minEpoch = 100ms;
+
+template<typename T>
+int BenchVector(ankerl::nanobench::Bench& bench, bool withNoReserve)
+{
+#ifdef NDEBUG
+	const size_t nbPushBacksPerIteration[] = { 1'000, 100'000, 10'000'000 };
+#else
+	const size_t nbPushBacksPerIteration[] = { 1'000 };
+#endif
+	uint64_t x = 1;
+	{
+		std::vector<T> vector;
+		for (size_t size : nbPushBacksPerIteration)
+		{
+			vector.reserve(size); // Don't measure allocator
+			bench.complexityN(size)
+				.minEpochTime(minEpoch)
+				.run("std::vector.push_back", [&] {
+				for (int i = 0; i < size; i++)
+				{
+					vector.push_back({ x });
+				}
+				ankerl::nanobench::doNotOptimizeAway(x += vector.size());
+				vector.clear();
+					});
+		}
+	}
+
+	{
+		ExampleGuardedVector<T> guardedvector;
+		for (size_t size : nbPushBacksPerIteration)
+		{
+			guardedvector.reserve(size); // Don't measure allocator
+			bench.complexityN(size)
+				.minEpochTime(minEpoch)
+				.run("guardedvector.push_back", [&] {
+				for (int i = 0; i < size; i++)
+				{
+					guardedvector.push_back({ x });
+				}
+				ankerl::nanobench::doNotOptimizeAway(x += guardedvector.size());
+				guardedvector.clear();
+					});
+		}
+	}
+
+	if (withNoReserve)
+	{
+		{
+			std::vector<T> vector;
+			for (size_t size : nbPushBacksPerIteration)
+			{
+				bench.complexityN(size)
+					.minEpochTime(minEpoch)
+					.run("std::vector.push_back - noreserve", [&] {
+					for (int i = 0; i < size; i++)
+					{
+						vector.push_back({ x });
+					}
+					ankerl::nanobench::doNotOptimizeAway(x += vector.size());
+					vector.clear();
+					vector.shrink_to_fit();
+						});
+			}
+		}
+
+		{
+			ExampleGuardedVector<T> guardedvector;
+			for (size_t size : nbPushBacksPerIteration)
+			{
+				bench.complexityN(size)
+					.minEpochTime(minEpoch)
+					.run("guardedvector.push_back - noreserve", [&] {
+					for (int i = 0; i < size; i++)
+					{
+						guardedvector.push_back({ x });
+					}
+					ankerl::nanobench::doNotOptimizeAway(x += guardedvector.size());
+					guardedvector.clear();
+					guardedvector.shrink_to_fit();
+						});
+			}
+		}
+	}
+	return x;
+}
+
+int main() {
+	
+	int x = 0;
+
+	x += BenchVector<uint64_t>(ankerl::nanobench::Bench().title("Vector of uint64_t"), true);
+	struct Payload16B {
+		uint64_t storage[2];
+	};
+	x += BenchVector<Payload16B>(ankerl::nanobench::Bench().title("Vector of uint64_t*2"), false);
+	struct Payload32B {
+		uint64_t storage[4];
+	};
+	x += BenchVector<Payload32B>(ankerl::nanobench::Bench().title("Vector of uint64_t*4"), false);
+	struct PayloadString {
+		PayloadString(uint64_t v) {
+			(void)v;
+		}
+		std::string storage;
+	};
+	x += BenchVector<PayloadString>(ankerl::nanobench::Bench().title("Vector of std::string"), false);
+	return x;
+}
--- examples/GuardedVectorExample.h
+++ examples/GuardedVectorExample.h
@@ -1,27 +1,18 @@
 ﻿#include <BadAccessGuards.h>
 #include <vector>
 
-
 // Very small example of implementation for a vector type with a reduced number of methods.
 // This is NOT the inteded way to use the library, ideally you would add the guards to the implementation of the container itself!
 // Here we are paying the cost of passing things around, especially in Debug builds.
 template<typename T>
-class ExampleGuardedVector
+class ExampleGuardedVector : public std::vector<T>
 {
-	std::vector<T> impl;
+	using super = std::vector<T>;
 	BA_GUARD_DECL(BAShadow);
 public:
 	ExampleGuardedVector(){}
-	ExampleGuardedVector(ExampleGuardedVector&& rhs)
-	{
-		BA_GUARD_WRITE(BAShadow);
-		impl = std::move(rhs.impl);
-	}
-	ExampleGuardedVector(const ExampleGuardedVector& rhs)
-	{
-		BA_GUARD_WRITE(BAShadow);
-		impl = rhs.impl;
-	}
+	ExampleGuardedVector(const ExampleGuardedVector& rhs) : super(rhs) { }
+	ExampleGuardedVector(ExampleGuardedVector&& rhs) : super(std::move(rhs)) { }
 
 	~ExampleGuardedVector()
 	{
@@ -31,74 +22,74 @@ public:
 	ExampleGuardedVector& operator=(ExampleGuardedVector&& rhs)
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl = std::move(rhs.impl);
+		super::operator=(std::move(rhs));
 		return *this;
 	}
-
+	
 	ExampleGuardedVector& operator=(const ExampleGuardedVector& rhs)
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl = rhs.impl;
+		super::operator=(rhs);
 		return *this;
 	}
 
 	void push_back(const T& val) {
 
 		BA_GUARD_WRITE(BAShadow);
-		impl.push_back(val);
+		super::push_back(val);
 	}
 
 	void push_back(T&& val)
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl.push_back(std::move(val));
+		super::push_back(std::move(val));
 	}
 
 	T* data()
 	{
 		BA_GUARD_READ(BAShadow);
-		return impl.data();
+		return super::data();
 	}
 	const T* data() const
 	{
 		BA_GUARD_READ(BAShadow);
-		return impl.data();
+		return super::data();
 	}
 
 	size_t size() const
 	{
 		BA_GUARD_READ(BAShadow);
-		return impl.size();
+		return super::size();
 	}
 
 	size_t capacity() const
 	{
 		BA_GUARD_READ(BAShadow);
-		return impl.capacity();
+		return super::capacity();
 	}
 
 	void resize(size_t newSize)
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl.resize(newSize);
+		super::resize(newSize);
 	}
 
 	void reserve(size_t newCapacity)
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl.reserve(newCapacity);
+		super::reserve(newCapacity);
 	}
 
 	void shrink_to_fit()
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl.shrink_to_fit();
+		super::shrink_to_fit();
 	}
 
 	void clear() noexcept
 	{
 		BA_GUARD_WRITE(BAShadow);
-		impl.clear();
+		super::clear();
 	}
 
 	T& operator[](size_t index) noexcept
@@ -121,22 +112,22 @@ public:
 	{
 		// We can't know whether it is used as read only or wrote to, accept the limitation and err on the conservative size
 		BA_GUARD_READ(BAShadow);
-		return impl.data();
+		return super::data();
 	}
 	const T* begin() const noexcept
 	{
 		BA_GUARD_READ(BAShadow);
-		return impl.data();
+		return super::data();
 	}
 	T* end() noexcept
 	{
 		// We can't know whether it is used as read only or wrote to, accept the limitation and err on the conservative size
 		BA_GUARD_READ(BAShadow);
-		return impl.data() + impl.size();
+		return super::data() + super::size();
 	}
 	const T* end() const noexcept
 	{
 		BA_GUARD_READ(BAShadow);
-		return impl.data() + impl.size();
+		return super::data() + super::size();
 	}
 };
